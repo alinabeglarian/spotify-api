eb16c3af6d51557cd0da249721b55aa71dd0fe03
alinabeglarian <alinabeglarian@gmail.com>
2019-06-29 13:24:14 +0200

    playlist endpoints

--- playlist/router.js
+++ playlist/router.js
@@ -7,7 +7,9 @@ router.post(
   '/playlists',
   (req, res, next) => Playlist
     .create(req.body)
-    .then(playlist => res.status(201).json(playlist))
+    .then(playlist => res
+      .status(201)
+      .json({New_Playlist: playlist}))
     .catch(error => next(error))
 )
 
@@ -15,11 +17,34 @@ router.get(
   '/playlists', 
   (req, res, next) => Playlist
     .findAll()
-    .then(playlists => res.send(playlists))
+    .then(playlists => res.json({Playlists: playlists}))
     .catch(error => next(error))
 )
 
+router.get(
+  '/playlists/:id',
+  (req, res, next) => {
+  const id = req.params.id
+  Playlist
+    .findByPk(id)
+    .then(playlist => res.json({Playlist: playlist}))
+    .catch(error => next(error))
+  }
+)
 
+router.delete(
+  '/playlists/:id',
+  (req, res, next) => {
+  const id = req.params.id
+  Playlist
+    .findByPk(id)
+    .then(playlist => playlist.destroy({playlist}) )
+    .then(playlist => res
+      .status(200)
+      .json({message: 'Playlist had succesfully been deleted'}))
+    .catch(error => next(error))
+  }
+)
 
 
 module.exports = router
